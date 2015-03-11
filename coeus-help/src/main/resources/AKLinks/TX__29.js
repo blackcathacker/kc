8f1c910689ba6bc323339a57e2b3811038ef099e
blackcathacker <douglas.pace@gmail.com>
2015-03-10 17:40:54 -0700

    RESKC-201:Update Help Files licenses

--- coeus-help/src/main/resources/AKLinks/TX__29.js
+++ coeus-help/src/main/resources/AKLinks/TX__29.js
@@ -1,1 +1,19 @@
+/*
+ * Kuali Coeus, a comprehensive research administration system for higher education.
+ * 
+ * Copyright 2005-2015 Kuali, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 ﻿d2hStoreMenuItems("TX__29", [["Documents/overview.htm", "right", "Overview"],["Documents/overview1.htm", "right", "Overview"],["Documents/overview2.htm", "right", "Overview"],["Documents/overview3.htm", "right", "Overview"],["Documents/overview4.htm", "right", "Overview"],["Documents/overview5.htm", "right", "Overview"],["Documents/overview6.htm", "right", "Overview"]]);
